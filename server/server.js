b31be2df8edb39cc6efe271471c65404484a89af
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-08-29 20:16:56 -0500

    Add: basic middlewares

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 const port = process.env.PORT || 2030;
 
