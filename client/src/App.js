b8664a246764c6f9b8ab8d6883812dc96081dd50
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-09-07 23:08:33 -0500

    Add: album page and separate get album function

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Wrapper from './components/Wrapper';
 import { Fragment } from 'react';
 import HomePage from './pages/HomePage';
+import AlbumPage from './pages/AlbumPage';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/albums/new' element={<h2>New Album Page</h2>} />
-          <Route path='/albums/:slug' element={<h2>Album Page</h2>} />
+          <Route path='/albums/:slug' element={<AlbumPage />} />
           <Route path='/login' element={<h2>Login Page</h2>} />
           <Route path='*' element={<h2>Not Found</h2>} />
         </Routes>
