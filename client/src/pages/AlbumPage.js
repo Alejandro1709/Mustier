da48311c6619f68959dbb7dc57fe4772c12cffee
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-09-10 20:54:54 -0500

    Add: fetch an album by its slug

--- client/src/pages/AlbumPage.js
+++ client/src/pages/AlbumPage.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { getAlbumBySlug } from '../albums';
+import axios from 'axios';
 
 function AlbumPage() {
+  const [album, setAlbum] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
   const { slug } = useParams();
-  const album = getAlbumBySlug(slug);
+
+  const fetchAlbumBySlug = async (slug) => {
+    setIsLoading(true);
+
+    try {
+      const { data } = await axios.get(
+        `http://localhost:2030/api/v1/albums/${slug}`
+      );
+      setAlbum(data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchAlbumBySlug(slug);
+
+    return () => {
+      setAlbum({});
+    };
+  }, [slug]);
+
+  if (isLoading) return <p>Loading album...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div>
-      <h1>{album.title} Page</h1>
+      <h1>{album.albumTitle} Page</h1>
     </div>
   );
 }
