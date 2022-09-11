096c6e538d6ffe7304c52e95a34da940b5f60276
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-09-11 16:16:22 -0500

    Add: delete song

--- client/src/components/SongRow.js
+++ client/src/components/SongRow.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function SongRow({ num, song, onSongEdition, handleAddSongToParams }) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const navigate = useNavigate();
+
   const handleSongEdition = () => {
     handleAddSongToParams(song._id);
     onSongEdition(true);
   };
+
+  const handleEditSong = async () => {
+    // eslint-disable-next-line no-restricted-globals
+    let ans = confirm('Are you sure?');
+
+    if (!ans) return;
+
+    setLoading(true);
+
+    try {
+      const { data } = await axios.delete(
+        `http://localhost:2030/api/v1/songs/${song._id}`
+      );
+      setLoading(false);
+      navigate('/');
+    } catch (error) {
+      setLoading(false);
+      setError(error);
+    }
+  };
+
   return (
     <div className='list__track'>
       <div className='track__left'>
@@ -16,7 +44,7 @@ function SongRow({ num, song, onSongEdition, handleAddSongToParams }) {
       <div className='track__right'>
         <b>{String(song.songDuration).replace('.', ':')}</b>
         <button onClick={handleSongEdition}>Edit</button>
-        <button>Delete</button>
+        <button onClick={handleEditSong}>Delete</button>
         <span>Reorder</span>
       </div>
     </div>
