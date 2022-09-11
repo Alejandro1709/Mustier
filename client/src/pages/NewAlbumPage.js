6cbcb10ab1658bd44d9405a2b8a33786d5e994b8
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-09-10 23:04:00 -0500

    Add: create album functionality done

--- client/src/pages/NewAlbumPage.js
+++ client/src/pages/NewAlbumPage.js
@@ -1,21 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function NewAlbumPage() {
+  const [albumTitle, setAlbumTitle] = useState('');
+  const [albumAuthor, setAlbumAuthor] = useState('');
+  const [albumReleaseDate, setAlbumReleaseDate] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleCreateAlbum = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const newAlbum = {
+      albumTitle,
+      albumAuthor,
+      albumReleaseDate,
+    };
+
+    try {
+      const { data } = await axios.post(
+        'http://localhost:2030/api/v1/albums',
+        JSON.stringify(newAlbum),
+        config
+      );
+
+      console.log(data);
+      navigate('/');
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
+
+  if (loading) return <p>Loading album...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div className='new-albumPage'>
       <h1>New Album</h1>
-      <form>
+      <form onSubmit={handleCreateAlbum}>
         <div className='form-group'>
           <label htmlFor='albumTitle'>Album Title:</label>
-          <input type='text' name='albumTitle' id='albumTitle' />
+          <input
+            type='text'
+            name='albumTitle'
+            id='albumTitle'
+            value={albumTitle}
+            onChange={(e) => setAlbumTitle(e.target.value)}
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='albumAuthor'>Album Author:</label>
-          <input type='text' name='albumAuthor' id='albumAuthor' />
+          <input
+            type='text'
+            name='albumAuthor'
+            id='albumAuthor'
+            value={albumAuthor}
+            onChange={(e) => setAlbumAuthor(e.target.value)}
+          />
         </div>
         <div className='form-group'>
           <label htmlFor='albumReleaseDate'>Album Release Date:</label>
-          <input type='date' name='albumReleaseDate' id='albumReleaseDate' />
+          <input
+            type='date'
+            name='albumReleaseDate'
+            id='albumReleaseDate'
+            value={albumReleaseDate}
+            onChange={(e) => setAlbumReleaseDate(e.target.value)}
+          />
         </div>
         <button className='submit-btn' type='submit'>
           Create Album
