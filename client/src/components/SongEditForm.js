07d9b6cc14597f86d8b794d2b3a2c9f865d9a6bf
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-09-11 12:47:57 -0500

    Add: Modal for editing a song

--- client/src/components/SongEditForm.js
+++ client/src/components/SongEditForm.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { AiOutlineCheck, AiOutlineClose } from 'react-icons/ai';
 
 function SongEditForm() {
-  return <div>SongEditForm</div>;
+  return (
+    <div className='edit-songForm'>
+      <form className='edit-songForm__wrapper'>
+        <input type='text' placeholder='Song Title' />
+        <input type='number' placeholder='3.4' />
+        <button className='confirm-btn' type='submit'>
+          <AiOutlineCheck />
+        </button>
+        <button className='dismiss-btn' type='button'>
+          <AiOutlineClose />
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default SongEditForm;
