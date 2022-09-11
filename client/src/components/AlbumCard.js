81b6bae2bbfd26b890a6fd12408247f400cc178c
Alejandro La Rosa <alejandro.lr.developer@hotmail.com>
2022-09-10 20:49:00 -0500

    Fetch albums from API and extact year from release date

--- client/src/components/AlbumCard.js
+++ client/src/components/AlbumCard.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { getYear } from 'date-fns';
+import parseISO from 'date-fns/parseISO';
 import PropTypes from 'prop-types';
 
 function AlbumCard({ album }) {
+  const parseDate = (dateStr) => {
+    const date = parseISO(dateStr);
+    return getYear(date);
+  };
+
   return (
     <div className='list__item'>
       <div className='item__wrapper'>
@@ -10,10 +17,15 @@ function AlbumCard({ album }) {
           <img src={album.imageSrc} alt='bf cover' draggable={false} />
         </div>
         <div className='wrapper__details'>
-          <h3>{album.title} - 2015</h3>
-          <p>{album.author}</p>
+          <h3>
+            {album.albumTitle} - {parseDate(album.albumReleaseDate)}
+          </h3>
+          <p>{album.albumAuthor}</p>
         </div>
-        <NavLink to={`/albums/${album.slug}`} className='wrapper__actions link'>
+        <NavLink
+          to={`/albums/${album.albumSlug}`}
+          className='wrapper__actions link'
+        >
           See More
         </NavLink>
       </div>
--- client/src/components/AlbumList.js
+++ client/src/components/AlbumList.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AlbumCard from './AlbumCard';
+import axios from 'axios';
 
-function AlbumList({ albums }) {
+function AlbumList() {
+  const [albums, setAlbums] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleFetchAlbums = async () => {
+    setIsLoading(true);
+
+    try {
+      const { data } = await axios.get('http://localhost:2030/api/v1/albums');
+      setAlbums(data);
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    handleFetchAlbums();
+
+    return () => {
+      setAlbums([]);
+    };
+  }, []);
+
+  if (isLoading) return <p>Loading albums...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div className='homePage__list'>
       {albums.map((album) => (
-        <AlbumCard key={album.id} album={album} />
+        <AlbumCard key={album._id} album={album} />
       ))}
     </div>
   );
